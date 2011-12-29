157163656d8c547f5411278938c9d59fab490026
Jonathan <jon.bailey889@gmail.com>
2011-12-29 17:45:08 +0000

    Start of v2.0
    SKInboxItem, can't be used at all.

---
+++ Classes/SKInboxItem.h
@@ -0,0 +1,13 @@
+//
+//  SKInboxItem.h
+//  StackKit
+//
+//  Created by Jonathan Bailey on 29/12/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SKObject.h"
+
+@interface SKInboxItem : SKObject
+
+@end
---
+++ Classes/SKInboxItem.m
@@ -0,0 +1,13 @@
+//
+//  SKInboxItem.m
+//  StackKit
+//
+//  Created by Jonathan Bailey on 29/12/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SKInboxItem.h"
+
+@implementation SKInboxItem
+
+@end
